ce33f608e0623934b121d664f5556a51ddab059e
Yewonderful <114788251+ypa53@users.noreply.github.com>
2025-10-04 17:02:26 -0700

    use javascript

--- client/src/javascript/editpage.js
+++ client/src/javascript/editpage.js
@@ -1,10 +1,10 @@
 const menuButton = document.getElementById("menu-btn");
 const dropdown = document.getElementById("dropdown-content");
 
-menuButton?.addEventListener("click", () => {
-  if (dropdown?.style.display === "block") {
+menuButton.addEventListener("click", () => {
+  if (dropdown.style.display === "block") {
     dropdown.style.display = "none";
   } else {
-    dropdown!.style.display = "block";
+    dropdown.style.display = "block";
   }
 });
